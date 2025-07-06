1ae90c32ce234ccbeceb479b4214bb65e53a25a6
DavidinhoHD <davidinho.minecraft@gmail.com>
2025-07-06 20:10:55 +0200

    Fix typos in variable names for content summary query and cleaned search query

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var (
 	systemPrompt = `You are a helpful assistant that generates search queries based on user questions. Only generate one search query.`
 	userQuestion = `What is the recent news in physics this week`
-	contentSummaryQuerry = `You are a helpful assistant that briefly summarizes the content of a webpage. Summarize the users input.`
+	contentSummaryQuery = `You are a helpful assistant that briefly summarizes the content of a webpage. Summarize the users input.`
 )
 
 func main() {
@@ -27,10 +27,10 @@ func main() {
 
 	// generate search query based on user question
 	searchQuery := ollamaRequest(llmAPI, fmt.Sprintf("%s\n%s", systemPrompt, userQuestion))
-	cleandSearchQuery := removeThinkingTags(searchQuery.Response)
+	cleanedSearchQuery := removeThinkingTags(searchQuery.Response)
 
 	// search for articles 
-	articles := exaSearchRequest(exaKey, cleandSearchQuery)
+	articles := exaSearchRequest(exaKey, cleanedSearchQuery)
 	var results []string
 	for _, res:= range articles.Results {
 		results = append(results, res.URL)
@@ -44,8 +44,8 @@ func main() {
 	}
 
 	// summarize content via LLM
-	summaryResponse := ollamaRequest(llmAPI, fmt.Sprintf("%s\n%s", contentSummaryQuerry, strings.Join(content, "\n")))
-	cleandSummaryResponse := removeThinkingTags(summaryResponse.Response)
+	summaryResponse := ollamaRequest(llmAPI, fmt.Sprintf("%s\n%s", contentSummaryQuery, strings.Join(content, "\n")))
+	cleanedSummaryResponse := removeThinkingTags(summaryResponse.Response)
 
-	fmt.Println(cleandSummaryResponse)
+	fmt.Println(cleanedSummaryResponse)
 }
